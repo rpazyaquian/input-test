5adf3c77b70b82c1701f0468efe5798a3c3aeb57
rpazyaquian <rpaz@localytics.com>
2015-02-25 10:48:25 -0500

    added input image assets
    instantiated image of dpad in setup
    
    replaced black dpad with white dpad; drew dpad to 0, 0

--- main.rb
+++ main.rb
@@ -3,12 +3,16 @@ require 'gosu'
 class GameWindow < Gosu::Window
 	def initialize
 		super(640, 480, false)
+
+		@dpad_image = Gosu::Image.new(self, "./assets/img/DPAD.png")
+
 	end
 
 	def update
 	end
 
 	def draw
+		@dpad_image.draw(0, 0, 0)
 	end
 end
 
