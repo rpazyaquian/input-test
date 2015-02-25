e28a40e54c3114f47b8430aaae314c9e60155d55
rpazyaquian <rpaz@localytics.com>
2015-02-25 11:02:35 -0500

    added checks for input state
    added 'nothing' case for no direction held down, rudimentary debugging shit, not sure if this does quite what i want it to do

--- main.rb
+++ main.rb
@@ -5,10 +5,20 @@ class GameWindow < Gosu::Window
 		super(640, 480, false)
 
 		@dpad_image = Gosu::Image.new(self, "./assets/img/DPAD.png")
-
 	end
 
 	def update
+		if button_down? Gosu::KbUp
+			puts "up"
+		elsif button_down? Gosu::KbDown
+			puts "down"
+		elsif button_down? Gosu::KbLeft
+			puts "left"
+		elsif button_down? Gosu::KbRight
+			puts "right"
+		else
+			puts "nothing"
+		end
 	end
 
 	def draw
