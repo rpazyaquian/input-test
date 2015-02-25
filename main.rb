c04297ae6c68fd15127ed6ab1280684732718939
rpazyaquian <rpaz@localytics.com>
2015-02-25 10:39:36 -0500

    created game window class
    instatiated game window, now looping

---
+++ main.rb
@@ -0,0 +1,16 @@
+require 'gosu'
+
+class GameWindow < Gosu::Window
+	def initialize
+		super(640, 480, false)
+	end
+
+	def update
+	end
+
+	def draw
+	end
+end
+
+window = GameWindow.new
+window.show
